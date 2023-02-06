ae5ab41ab9aefc93170b9920c9e1758d232bea0f
xfsaw <hexma.m32@gmail.com>
2023-02-05 16:33:23 -0800

    push

---
+++ DeepMaw/DeepMaw.cpp
@@ -0,0 +1,12 @@
+﻿// DeepMaw.cpp : Defines the entry point for the application.
+//
+
+#include "DeepMaw.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello, CMake." << endl;
+	return 0;
+}
